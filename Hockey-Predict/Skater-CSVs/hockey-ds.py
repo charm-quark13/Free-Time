edece312320c9667748c14e57e8c56791b4a6252
charm-quark13 <epluhar3@gmail.com>
2018-07-12 13:05:48 -0500

    functions in skaters and advanced stats

---
+++ Hockey-Predict/Skater-CSVs/hockey-ds.py
@@ -0,0 +1,47 @@
+import os
+import pandas as pd
+import numpy as np
+
+def dfread(filename):
+    df = pd.read_csv(str(filename))
+    return df
+
+def filefind(filename):
+    filepath = str(os.getcwd()) + '/'
+    direction = filepath + str(filename)
+    return direction
+
+def Pos_Org(dframe):
+    d_data = dframe.groupby('Pos').get_group('D')
+    sel_data = []
+    for position in ['LW','RW','C']:
+        sel_data.append(dframe.groupby('Pos').get_group(position))
+    f_data = pd.concat(sel_data)
+    return f_data, d_data
+
+filename = filefind('skaters-2009.csv')
+df = dfread(filename)
+
+filename = filefind('skaters-adv-2009.csv')
+adv_df = dfread(filename)
+
+#  Need to get rid of duplicate rows in f_df and d_df before concatenating
+#  with the advanced statistics tables.
+df = df.drop_duplicates(subset = 'Player',keep = 'first')
+df = df.reset_index(drop=True)
+df = df.drop('Unnamed: 0',axis=1)
+adv_df = adv_df.drop('Unnamed: 0',axis=1)
+
+df = df.set_index('Player',drop=False)
+adv_df = adv_df.set_index('Player',drop=False)
+
+adf_df, add_df = Pos_Org(adv_df)
+f_df, d_df = Pos_Org(df)
+
+f_df = pd.concat([f_df,adf_df],axis=1)
+d_df = pd.concat([d_df,add_df],axis=1)
+
+#print(adf_df)
+#print(add_df)
+print(f_df)
+print(d_df)
--- Hockey-Predict/hockey-parse.py
+++ Hockey-Predict/hockey-parse.py
@@ -12,10 +12,10 @@ dir_str = str(os.getcwd())
 for file in os.listdir(dir_str):
     filename = os.fsdecode(file)
     if filename.endswith('.html'):
-        if filename.find('goalies')!=-1:
+        if filename.find('skaters')!=-1:
             os.chdir(dir_str)
             print(filename)
-            hyphen = filename.find('-')
+            hyphen = filename.rfind('-')
             period = filename.find('.')
             with open(filename,'r') as doc:
                 parse = BS(doc,'lxml')
@@ -56,68 +56,6 @@ for file in os.listdir(dir_str):
 
                 df = pd.DataFrame(row_data,columns=column_title)
 
-                with open('goalies-'+ str(filename[hyphen+1:period]) + '.csv','w') as f:
-                    df.to_csv(path_or_buf=f)
-exit()
 
-#                n_rows = 0
-#                n_columns = 0
-#                point = 0
-#                for titles in tag.find_all('div',class_='heading'):
-#                    point += 1
-#                    if point == 1:
-#                        column_title = []
-#                        for columns in titles.find_all('div'):
-#                            column_title.append(str(columns.text.strip()))
-#                            n_columns += 1
-#                        column_title.append('Beer Styles')
-#                        column_title.insert(2,'Maximum ' + str(column_title[1]))
-#                        column_title[1] = 'Minimum ' + str(column_title[1])
-#                        #print(column_title)
-#                for row in tag.find_all('div',class_='content'):
-#                    point = 1
-#                    n_rows += 1
-#                    #print(tag.text.strip())
-#                if n_rows != 0:
-#                    df = pd.DataFrame(index=range(n_rows),columns=column_title)
-#                    row_point = 0
-#                    for row in tag.find_all('div',class_='content'):
-#                        col_point = 0
-#                        while col_point <= 5:
-#                            cols = str(row.get_text())
-#                            data_list = cols.splitlines()
-#                            data = []
-#                            for line in data_list:
-#                                if line.strip() != str(''):
-#                                    data.append(line.strip())
-#                            if len(data) == 3:
-#                                data.insert(2,str(''))
-#                            data.append(str(filename)[:-5])
-##                            print(data)
-#                            if col_point == 0:
-#                                df.iat[row_point,col_point] = data[col_point]
-#                            elif col_point == 1:
-#                                trim = data[1][:-1]
-#                                trim = trim.split(str('-'))
-#                                df.iat[row_point,col_point] = trim[0]
-#                            elif col_point == 2:
-#                                trim = data[1][:-1]
-#                                trim = trim.split(str('-'))
-#                                if len(trim) == 1:
-#                                    df.iat[row_point,col_point] = trim[0]
-#                                else:
-#                                    df.iat[row_point,col_point] = trim[1]
-#                            else:
-#                                df.iat[row_point,col_point] = data[col_point-1]
-#                            col_point +=1
-#                        row_point += 1
-##                    print(df)
-#                    poynt += 1
-#                    if poynt == 1:
-#                        df1 = df
-#                    else:
-#                        #df_master = pd.concat([df1,df])
-#                        df_master = df1.merge(df,how='outer')
-#                        df1 = df_master
-#
-#            continue
+                with open('skaters-adv-'+ str(filename[hyphen+1:period]) + '.csv','w') as f:
+                    df.to_csv(path_or_buf=f)
